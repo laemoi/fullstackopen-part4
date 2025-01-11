d51aa089a9b66101d179f5e81891a9cf51544e41
Lassi Oinas <lassi.oinas@hotmail.com>
2025-01-11 20:26:39 +0200

    4.4: helper functions, unit tests step2

---
+++ tests/list_helper.test.js
@@ -0,0 +1,57 @@
+const { test, describe } = require('node:test')
+const assert = require('node:assert')
+const listHelper = require('../utils/list_helper')
+
+test('dummy returns one', () => {
+  const blogs = []
+
+  const result = listHelper.dummy(blogs)
+  assert.strictEqual(result, 1)
+})
+
+describe('total likes', () => {
+  const emptyBlogList = []
+  const listWithOneBlog = [
+    {
+      _id: 'a000',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      likes: 5,
+      __v: 0
+    }
+  ]
+  const listWithMultipleBlogs = [
+    {
+      _id: 'a001',
+      title: 'Testing blogs',
+      author: 'Teemu Teekkari',
+      url: 'https://tietokilta.fi/fi',
+      likes: 99,
+      __v: 0
+    },
+    {
+      _id: 'a002',
+      title: 'Testing blogs again',
+      author: 'Tiina Teekkari',
+      url: 'https://tietokilta.fi/fi',
+      likes: 21,
+      __v: 0
+    }
+  ]
+
+  test('when list is empty, equals zero', () => {
+    const result = listHelper.totalLikes(emptyBlogList)
+    assert.strictEqual(result, 0)
+  })
+
+  test('when list has only one blog, equals the likes of that blog', () => {
+    const result = listHelper.totalLikes(listWithOneBlog)
+    assert.strictEqual(result, 5)
+  })
+
+  test('when list has multiple blogs, equals the sum of the likes of each blog', () => {
+    const result = listHelper.totalLikes(listWithMultipleBlogs)
+    assert.strictEqual(result, 120)
+  })
+})
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,5 +1,13 @@
 const dummy = (blogs) => 1
 
+const totalLikes = (blogs) => {
+  if (blogs.length > 0) {
+    return blogs.map(b => b.likes).reduce((sum, curr) => sum + curr, 0)
+  }
+  return 0
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
