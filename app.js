f1e7349934f5468f2660ad87714044effc3cbcb7
Lassi Oinas <lassi.oinas@hotmail.com>
2025-02-07 12:30:42 +0200

    4.15: bloglist expansions step3

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-const config = require('./utils/config.js')
-const blogRouter = require('./controllers/blogs.js')
 const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
+const config = require('./utils/config.js')
+const blogRouter = require('./controllers/blogs.js')
+const userRouter = require('./controllers/users.js')
 
 mongoose.connect(config.MONGODB_URI)
 
@@ -11,5 +12,6 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/api/blogs', blogRouter)
+app.use('/api/users', userRouter)
 
 module.exports = app
