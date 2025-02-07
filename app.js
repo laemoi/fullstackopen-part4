ea1938914e23f671477dca4e97771d107ae286dc
Lassi Oinas <lassi.oinas@hotmail.com>
2025-02-07 18:14:55 +0200

    4.18: bloglist expansions step6

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose')
 const config = require('./utils/config.js')
 const blogRouter = require('./controllers/blogs.js')
 const userRouter = require('./controllers/users.js')
+const loginRouter = require('./controllers/login.js')
 const middleware = require('./utils/middleware.js')
 
 mongoose.connect(config.MONGODB_URI)
@@ -15,6 +16,7 @@ app.use(express.json())
 
 app.use('/api/blogs', blogRouter)
 app.use('/api/users', userRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.errorHandler)
 
